da6868283c01214a3e44b30b172f96c13bf0285e
Luiz <doginuwu@gmail.com>
2022-01-13 17:24:31 -0300

    Settings update

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,13 +1,8 @@
-import 'dart:io';
-
 import 'package:anigiri/services/globals.dart' as globals;
 import 'package:anigiri/services/path.dart';
-import 'package:anigiri/widgets/default_app_bar.dart';
 import 'package:anigiri/widgets/dropdown_option.dart';
 import 'package:anigiri/widgets/go_back_app_bar.dart';
-import 'package:filesystem_picker/filesystem_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -17,26 +12,6 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  Future<void> changeDownloadPath(BuildContext context) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    Directory rootDir = await findRoot();
-
-    String? path = await FilesystemPicker.open(
-      title: 'Save to folder',
-      context: context,
-      rootDirectory: rootDir,
-      fsType: FilesystemType.folder,
-      pickText: 'Save files to this folder',
-      folderIconColor: Colors.blue[500],
-    );
-
-    if (path != null) {
-      globals.downloadPath = path;
-      prefs.setString('downloadsPath', path);
-      setState(() {});
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,9 +41,10 @@ class _SettingsState extends State<Settings> {
                       ),
                     ),
                     const SizedBox(
-                      height: 20,
+                      height: 10,
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         const Align(
                           alignment: Alignment.centerLeft,
@@ -91,11 +67,20 @@ class _SettingsState extends State<Settings> {
                         ),
                         Column(
                           children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text("Choose download path"),
+                                ElevatedButton(
+                                  onPressed: () async {
+                                    await changeDownloadPath(context);
+                                    setState(() {});
+                                  },
+                                  child: const Text("Choose download folder"),
+                                ),
+                              ],
+                            ),
                             Text(globals.downloadPath),
-                            ElevatedButton(
-                              onPressed: () => changeDownloadPath(context),
-                              child: const Text("Choose download folder"),
-                            )
                           ],
                         ),
                       ],
--- lib/services/path.dart
+++ lib/services/path.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
+import 'package:filesystem_picker/filesystem_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:anigiri/services/globals.dart' as globals;
 
 Future<String> getPathToDownload() async {
   try {
@@ -26,4 +30,23 @@ Future<Directory> findRoot() async {
   var directory = Directory('/storage/emulated/0/');
 
   return directory;
+}
+
+Future<void> changeDownloadPath(BuildContext context) async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  Directory rootDir = await findRoot();
+
+  String? path = await FilesystemPicker.open(
+    title: 'Save to folder',
+    context: context,
+    rootDirectory: rootDir,
+    fsType: FilesystemType.folder,
+    pickText: 'Save files to this folder',
+    folderIconColor: Colors.blue[500],
+  );
+
+  if (path != null) {
+    globals.downloadPath = path;
+    prefs.setString('downloadsPath', path);
+  }
 }
